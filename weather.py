43845674263e9573e835bcacbbbfd1b1cd7e0abf
sharathmll <sharathsharath94804@gmail.com>
2026-08-02 20:33:02 +0530

    adding new file

---
+++ weather.py
@@ -0,0 +1,15 @@
+import requests as r
+api_key='71434fd24fd953e1ce5dedc27c9fbb5f'
+user_h=input("Enter the city name: ")
+
+
+weather_data=r.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_h}&units=imperial&APPID={api_key}")
+
+data=weather_data.json()
+if data['cod']==404:
+    print("No city found")
+else:
+    weather=data['weather'][0]['main']
+    temp=data['main']['temp']
+    print(f'The weather in {user_h} is: {weather}')
+    print(f"The temperature of {user_h} is {temp}")
